c2717bf502c9680cf0d0881e3ec6070c169d46ec
Matt Payne <Payne@MattPayne.org>
2015-03-17 18:58:51 -0500

    Added Scott's comment to Util

--- src/main/groovy/Util.groovy
+++ src/main/groovy/Util.groovy
@@ -1,4 +1,5 @@
 class Util {
+    // This works in both gradle and eclipse.   Very nice!
     File stringToClassPathFile(String fileName) {
 		new File(getClass().getClassLoader().getResource(fileName).getFile());
     }
